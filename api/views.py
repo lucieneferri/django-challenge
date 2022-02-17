134b94af22a4adb22768689c848fcafc00b13a0d
Luciene Ferri <luciene_ferri@hotmail.com>
2022-02-17 10:58:37 -0300

    viewsets set up

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from serializers import UserSerializer, AuthorSerializer, ArticleSerializer
+from models import User, Author, Article
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    
+
