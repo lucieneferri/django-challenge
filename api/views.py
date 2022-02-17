1374da63ba69d512504f1d0872e7159174122bf6
Luciene Ferri <luciene_ferri@hotmail.com>
2022-02-17 18:01:23 -0300

    isadmin user

--- api/views.py
+++ api/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from serializers import UserSerializer, AuthorSerializer, ArticleSerializer
 from models import User, Author, Article
+from rest_framework.permissions import IsAdminUser
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 class AuthorViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
 
 class ArticleViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
 
-    
+
 
--- challenge/urls.py
+++ challenge/urls.py
@@ -7,14 +7,16 @@ from rest_framework.routers import DefaultRouter
 from api.views import UserViewSet, AuthorViewSet, ArticleViewSet
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
-router.register(r'user', UserViewSet)
+router.register(r'sign-up', UserViewSet)
 router.register(r'author', AuthorViewSet)
 router.register(r'article', ArticleViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router,urls)),
+    path('api/', include(router,urls)),
+    path('/api/login/', views.obtain_auth_token),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
