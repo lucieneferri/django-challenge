03f09662fb041bc7626101911cdb4c318f12047b
Luciene Ferri <luciene_ferri@hotmail.com>
2022-02-17 16:07:44 -0300

    url file

--- challenge/urls.py
+++ challenge/urls.py
@@ -1,21 +1,20 @@
-"""challenge URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django import urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.db import router
+from rest_framework.routers import DefaultRouter
+from api.views import UserViewSet, AuthorViewSet, ArticleViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+
+router = DefaultRouter()
+router.register(r'user', UserViewSet)
+router.register(r'author', AuthorViewSet)
+router.register(r'article', ArticleViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include(router,urls)),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
